b52358a774236555ed074204201d61e1dea8c3cd
vicecitydeluxe <vikvas95@gmail.com>
2022-09-05 16:54:02 +0300

    added some styles fixes

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h1 class="title">Welcome, {{ name }}!</h1>
+    <h4 class="title">Welcome, {{ name }} to the *marketplace name*. Use the buttons below to access commands.</h4>
     <div class="layout">
       <Button icon="pi pi-paypal" label="Top-up" iconPos="left" class="p-button-lg" @click="showPopupMessage"/>
         <Button icon="pi pi-upload" label="Upload" iconPos="left" class="p-button-lg" @click="showPopupAlert"/>
         <Button icon="pi pi-money-bill" label="Buy" iconPos="left" class="p-button-lg" @click="showConfirmMessage"/>
         <Button icon="pi pi-history" label="History" iconPos="left" class="p-button-lg"/>
         <Button icon="pi pi-question" label="Test" iconPos="left" class="p-button-lg"/>
-        <Button @click="previousMenu" icon="pi pi-forward" label="Back" iconPos="left" class="p-button-lg"/>
+        <Button @click="previousMenu" icon="pi pi-chevron-left" label="Back" iconPos="left" class="p-button-lg"/>
     </div>
   </div>
 </template>
@@ -25,10 +25,10 @@ export default {
       this.$store.commit('setName', '')
     },
     showPopupMessage() {
-      globalTelegram.showPopup({title: 'Title', message: 'Message'})
+      globalTelegram.showPopup({title: 'You can easily top up your balance', message: 'This feature would be added soon'})
     },
     showPopupAlert() {
-      globalTelegram.showAlert('Testing alert')
+      globalTelegram.showAlert('This feature would be added soon')
     },
     showConfirmMessage() {
       globalTelegram.showConfirm('Testing confirm')
@@ -52,5 +52,6 @@ export default {
 .title {
   display: flex;
   justify-content: center;
+  text-align: center;
 }
 </style>
