2f17d6cf7afcb3226f616dec7fa21b4c19784da6
vicecitydeluxe <vikvas95@gmail.com>
2022-09-07 10:38:28 +0300

    added some blank code for future: -axios base url -actions with GET and POST

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -3,11 +3,11 @@
     <h4 class="title">Welcome, {{ name }} to the *marketplace name*. Use the buttons below to access commands.</h4>
     <div class="layout">
       <Button icon="pi pi-paypal" label="Top-up" iconPos="left" class="p-button-lg" @click="showPopupMessage"/>
-        <Button icon="pi pi-upload" label="Upload" iconPos="left" class="p-button-lg" @click="showPopupAlert"/>
-        <Button icon="pi pi-money-bill" label="Buy" iconPos="left" class="p-button-lg" @click="showConfirmMessage"/>
-        <Button icon="pi pi-history" label="History" iconPos="left" class="p-button-lg"/>
-        <Button icon="pi pi-question" label="Test" iconPos="left" class="p-button-lg"/>
-        <Button @click="previousMenu" icon="pi pi-chevron-left" label="Back" iconPos="left" class="p-button-lg"/>
+      <Button icon="pi pi-upload" label="Upload" iconPos="left" class="p-button-lg" @click="showPopupAlert"/>
+      <Button icon="pi pi-money-bill" label="Buy" iconPos="left" class="p-button-lg" @click="showConfirmMessage"/>
+      <Button icon="pi pi-history" label="History" iconPos="left" class="p-button-lg"/>
+      <Button icon="pi pi-question" label="Test" iconPos="left" class="p-button-lg"/>
+      <Button @click="previousMenu" icon="pi pi-chevron-left" label="Back" iconPos="left" class="p-button-lg"/>
     </div>
   </div>
 </template>
@@ -19,13 +19,25 @@ const globalTelegram = window.Telegram.WebApp
 
 export default {
   name: "Layout",
+  data() {
+    return {
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      })
+    }
+  },
   methods: {
     previousMenu() {
       this.$parent.layoutVisible = false
       this.$store.commit('setName', '')
     },
     showPopupMessage() {
-      globalTelegram.showPopup({title: 'You can easily top up your balance', message: 'This feature would be added soon'})
+      globalTelegram.showPopup({
+        title: 'You can easily top up your balance',
+        message: 'This feature would be added soon'
+      })
     },
     showPopupAlert() {
       globalTelegram.showAlert('This feature would be added soon')
@@ -33,6 +45,12 @@ export default {
     showConfirmMessage() {
       globalTelegram.showConfirm('Testing confirm')
     },
+    // testFake() {
+    //   this.$store.dispatch('GET_TODOS')
+    // },
+    // testPost(payload) {
+    //   this.$store.dispatch('SEND_DATA', payload)
+    // }
   },
   computed: {
     ...mapGetters(['name'])
--- src/store/main.module.js
+++ src/store/main.module.js
@@ -1,18 +1,59 @@
+// import $axios from '../api/index'
+
 const state = {
     name: 'Unknown_user',
-
+    // todos: []
 }
 const getters = {
     name() {
         return state.name
     }
 }
-const actions = {}
+const actions = {
+
+    // GET_TODOS() {
+    //     return new Promise((resolve, reject) => {
+    //         $axios({
+    //             url: '/todos/1',
+    //             method: 'GET',
+    //         })
+    //             .then((resp) => {
+    //                 // this.todos = resp
+    //                 this.commit('setTodos', resp.data);
+    //                 resolve(resp)
+    //             })
+    //             .catch((err) => {
+    //                 reject(err)
+    //             })
+    //     })
+    // },
+
+    // SEND_DATA(context, payload) {
+    //     // const {test, dataToDestruct} = payload;
+    //     return new Promise((resolve, reject) => {
+    //         $axios({
+    //             url: `/posts`,
+    //             method: "POST",
+    //             data: payload
+    //         }).then(res => {
+    //             console.log(res.data)
+    //             resolve(true)
+    //         }).catch(err => {
+    //             console.log(err)
+    //             reject(err)
+    //         })
+    //     })
+    // },
+
+}
 
 const mutations = {
     setName(ctx, data) {
         state.name = data
-    }
+    },
+    // setTodos(ctx, data) {
+    //     state.todos = data
+    // },
 }
 
 export default {state, getters, actions, mutations}
