712efbe94edcfe202dcea27f74c212ff025e1a64
vicecitydeluxe <vikvas95@gmail.com>
2022-09-14 16:19:17 +0300

    default component behaviour quickfix

--- src/components/CountryMapper.vue
+++ src/components/CountryMapper.vue
@@ -56,6 +56,11 @@ export default {
       },
     }
   },
+  mounted() {
+    globalTelegram.expand()
+    globalTelegram.enableClosingConfirmation()
+    globalTelegram.MainButton.hide()
+  },
 }
 </script>
 
