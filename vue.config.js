2f70a2e39d185684cd95f0026ce2251cfe7e3805
vicecitydeluxe <vikvas95@gmail.com>
2022-09-19 09:43:28 +0300

    testing new deployment env.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: '/vue2/'
+    publicPath: '/coldleads-frontend/'
 })
 /**
  * csv-loader frontend usage
