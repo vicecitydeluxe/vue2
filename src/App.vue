35e5b4fb93af6a94e08c39df5697e3fc1381c9d1
vicecitydeluxe <vikvas95@gmail.com>
2022-10-03 16:34:39 +0400

    added global night mode auto switch: -night-mode in LoginForm is now also available

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,39 @@
 </template>
 
 <script>
-import LoginForm from "@/components/LoginForm";
-import Layout from "@/components/Layout";
+const globalTelegram = window.Telegram.WebApp;
 
 export default {
-  components: {LoginForm, Layout},
-
-}
+  data() {
+    return {
+      globalTelegram: globalTelegram,
+      darkModeSwitch: false,
+    };
+  },
+  watch: {
+    globalTelegram: {
+      handler(newValue) {
+        if (newValue.colorScheme === "light") {
+          this.darkModeSwitch = false;
+        } else if (newValue.colorScheme === "dark") {
+          this.darkModeSwitch = true;
+        }
+      },
+      deep: true,
+    },
+    darkModeSwitch: {
+      handler(newValue) {
+        if (newValue) {
+          document.body.className = "dark";
+        } else if (!newValue) {
+          document.body.className = "light";
+        }
+      },
+    },
+  },
+};
 </script>
+
+<style lang="scss">
+@import "/src/index.scss";
+</style>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -7,7 +7,7 @@
           <div class="p-field">
             <div class="p-float-label">
               <InputText id="name" v-model="userInfo.username" @keyup="updateUsername"
-                         :class="[validInput ? 'p-inputtext ' : 'p-invalid']"
+                :class="[validInput ? 'p-inputtext ' : 'p-invalid']"
               />
               <label for="name">You may choose alternative
                 username*</label>
@@ -29,6 +29,8 @@ export default {
   name: "LoginForm",
   data() {
     return {
+      globalTelegram: globalTelegram,
+      darkModeSwitch: false,
       showComponent: false,
       userInfo: {
         username: '',
@@ -78,14 +80,67 @@ export default {
       return this.validInput && this.userInfo.username
     }
   },
-  // watch: {
-  //   userInfo: {
-  //     handler(newValue) {
-  //       localStorage.setItem("userInfo", JSON.stringify(newValue));
-  //     },
-  //     deep: true
-  //   }
-  // },
+  watch: {
+    globalTelegram: {
+      handler(newValue) {
+        if (newValue.colorScheme === 'light') {
+         this.darkModeSwitch = false
+        } else if (newValue.colorScheme === 'dark') {
+         this.darkModeSwitch = true
+        }
+      },deep:true 
+    },
+    darkModeSwitch: {
+      handler(newValue) {
+        if (newValue) {
+          document.querySelectorAll('.p-fluid').forEach(e => e.classList.replace('p-fluid', 'p-fluid-dark'))
+          document.querySelectorAll('.p-inputtext').forEach(e => e.classList.replace('p-inputtext', 'p-inputtext-dark'))
+          document.querySelectorAll('.p-float-label').forEach(e => e.classList.replace('p-float-label', 'p-float-label-dark'))
+          document.querySelectorAll('.p-filled').forEach(e => e.classList.replace('p-filled', 'p-filled-dark'))
+          document.querySelectorAll('.p-inputwrapper-focus').forEach(e => e.classList.replace('p-inputwrapper-focus', 'p-inputwrapper-focus-dark'))
+          document.querySelectorAll('.p-inputwrapper-filled').forEach(e => e.classList.replace('p-inputwrapper-filled', 'p-inputwrapper-filled-dark'))
+          document.querySelectorAll('.p-button').forEach(e => e.classList.replace('p-button', 'p-button-dark'))
+          document.querySelectorAll('.p-component').forEach(e => e.classList.replace('p-component', 'p-component-dark'))
+        } if (!newValue) {
+          document.querySelectorAll('.p-fluid-dark').forEach(e => e.classList.replace('p-fluid-dark', 'p-fluid'))
+          document.querySelectorAll('.p-inputtext-dark').forEach(e => e.classList.replace('p-inputtext-dark', 'p-inputtext'))
+          document.querySelectorAll('.p-float-label-dark').forEach(e => e.classList.replace('p-float-label-dark', 'p-float-label'))
+          document.querySelectorAll('.p-filled-dark').forEach(e => e.classList.replace('p-filled-dark', 'p-filled'))  
+          document.querySelectorAll('.p-inputwrapper-focus-dark').forEach(e => e.classList.replace('p-inputwrapper-focus-dark', 'p-inputwrapper-focus'))
+          document.querySelectorAll('.p-inputwrapper-filled-dark').forEach(e => e.classList.replace('p-inputwrapper-filled-dark', 'p-inputwrapper-filled'))
+          document.querySelectorAll('.p-button-dark').forEach(e => e.classList.replace('p-button-dark', 'p-button'))
+          document.querySelectorAll('.p-component-dark').forEach(e => e.classList.replace('p-component-dark', 'p-component'))
+        }
+      },
+    },
+    userInfo: {
+      handler(newValue) {
+        if (newValue.username.length < 5 && newValue.username.length !==0 && this.darkModeSwitch) {
+          setTimeout(() => {
+          document.querySelectorAll('.p-inputtext').forEach(e => e.classList.replace('p-inputtext', 'p-inputtext-dark'))
+          document.querySelectorAll('.p-component').forEach(e => e.classList.replace('p-component', 'p-component-dark'))
+          document.querySelectorAll('.p-filled').forEach(e => e.classList.replace('p-filled', 'p-filled-dark'))
+          document.querySelectorAll('.p-invalid').forEach(e => e.classList.replace('p-invalid', 'p-invalid-dark'))    
+          },0)
+        } else if (!newValue.username.length && this.darkModeSwitch) {
+          setTimeout(()=> {
+          document.querySelectorAll('.p-inputtext').forEach(e => e.classList.replace('p-inputtext', 'p-inputtext-dark'))
+          document.querySelectorAll('.p-component').forEach(e => e.classList.replace('p-component', 'p-component-dark'))
+          document.querySelectorAll('.p-filled').forEach(e => e.classList.replace('p-filled', 'p-filled-dark'))
+          document.querySelectorAll('.p-invalid').forEach(e => e.classList.replace('p-invalid', 'p-invalid-dark'))
+          }, 0)
+        } else if (newValue.username.length >= 5 && this.darkModeSwitch) {
+          setTimeout(()=> {
+          document.querySelectorAll('.p-inputtext').forEach(e => e.classList.replace('p-inputtext', 'p-inputtext-dark'))
+          document.querySelectorAll('.p-component').forEach(e => e.classList.replace('p-component', 'p-component-dark'))
+          document.querySelectorAll('.p-filled').forEach(e => e.classList.replace('p-filled', 'p-filled-dark'))
+          document.querySelectorAll('.p-invalid').forEach(e => e.classList.replace('p-invalid', 'p-invalid-dark'))
+          }, 0)
+        }
+      },
+      deep:true
+    },
+  },
 }
 </script>
 
