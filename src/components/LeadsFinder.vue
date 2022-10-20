36fdbf878a1b2b4ab679704d4dcc11949c32cb8f
vicecitydeluxe <vikvas95@gmail.com>
2022-10-20 14:18:32 +0400

    minor dark styles fix

--- src/components/LeadsFinder.vue
+++ src/components/LeadsFinder.vue
@@ -13,10 +13,11 @@
         <div class="filter_header">Filter</div>
         <div class="filter_container_wrapper">
           <div class="filter_header_divider">Type (Multiple select)</div>
-          <SelectButton v-model="selectedType"
-                        :options="selectedOptions"
-                        optionLabel="name"
-                        multiple
+          <SelectButton
+              v-model="selectedType"
+              :options="selectedOptions"
+              optionLabel="name"
+              multiple
           />
         </div>
         <div class="filter_container_wrapper">
@@ -38,9 +39,11 @@
         </div>
         <div class="filter_container_wrapper">
           <div class="field-checkbox">
-            <Checkbox id="binary"
-                      v-model="checked"
-                      :binary="true"/>
+            <Checkbox
+                id="binary"
+                v-model="checked"
+                :binary="true"
+            />
             <label for="binary">Exclude leads I bought or
               ready have (leads having the same email or phone)</label>
           </div>
@@ -62,34 +65,41 @@
         <div class="limit_header">Limit</div>
         <div class="limit_wrapper">
           <div class="field-radiobutton">
-            <RadioButton id="city1"
-                         name="city"
-                         value="demand"
-                         v-model="demand"/>
+            <RadioButton
+                id="city1"
+                name="city"
+                value="demand"
+                v-model="demand"
+            />
             <label style="margin-right: 34px;
              margin-left:4px;"
                    for="city1">I need</label>
           </div>
-          <InputText class="input"
-                     type="text"
-                     v-model="value1"
-                     :class="[ darkModeSwitch ? 'input_dark' : 'input']"
+          <InputText
+              class="input"
+              type="text"
+              v-model="value1"
+              :class="[ darkModeSwitch ? 'input_dark' : 'input']"
           />
           <span :style="{marginLeft: '.5em'}">leads</span>
         </div>
         <div class="limit_wrapper">
           <div class="field-radiobutton">
-            <RadioButton id="city1"
-                         name="city"
-                         value="maxAmount"
-                         v-model="maxAmount"/>
+            <RadioButton
+                id="city1"
+                name="city"
+                value="maxAmount"
+                v-model="maxAmount"
+            />
             <label style="margin-left:4px;"
                    for="city1">I spent max</label>
           </div>
-          <InputText :class="[ darkModeSwitch ? 'input_dark' : 'input']"
-                     type="text"
-                     v-model="value1"
-                     placeholder="$"/>
+          <InputText
+              :class="[ darkModeSwitch ? 'input_dark' : 'input']"
+              type="text"
+              v-model="value1"
+              placeholder="$"
+          />
           <span :style="{marginLeft: '.5em'}">balance:</span>
         </div>
       </div>
@@ -159,15 +169,24 @@ export default {
           document.querySelectorAll('.button_left').forEach(e => e.classList.add('button_left_dark'))
           document.querySelectorAll('.button_middle').forEach(e => e.classList.add('button_middle_dark'))
           document.querySelectorAll('.button_right').forEach(e => e.classList.add('button_right_dark'))
+          document.querySelectorAll('.p-radiobutton-box').forEach(e => e.classList.add('p-radiobutton-box_dark'))
+          document.querySelectorAll('.p-accordion-header-link').forEach(e => e.classList.add('p-accordion-header-link_dark'))
+          document.querySelectorAll('.p-accordion-content').forEach(e => e.classList.add('p-accordion-content_dark'))
         } else if (!newValue) {
-          document.querySelectorAll('.sticky_dark').forEach(e => e.classList.remove('sticky_dark'))
-          document.querySelectorAll('.sticky_number__title_dark').forEach(e => e.classList.remove('sticky_number__title_dark'))
-          document.querySelectorAll('.filter_header_dark').forEach(e => e.classList.remove('filter_header_dark'))
-          document.querySelectorAll('.filter_header_divider_dark').forEach(e => e.classList.remove('filter_header_divider_dark'))
-          document.querySelectorAll('.filter_container_dark').forEach(e => e.classList.remove('filter_container_dark'))
+          document.querySelectorAll('.sticky').forEach(e => e.classList.remove('sticky_dark'))
+          document.querySelectorAll('.sticky_number__title').forEach(e => e.classList.remove('sticky_number__title_dark'))
+          document.querySelectorAll('.filter_header').forEach(e => e.classList.remove('filter_header_dark'))
+          document.querySelectorAll('.filter_header_divider').forEach(e => e.classList.remove('filter_header_divider_dark'))
+          document.querySelectorAll('.filter_container').forEach(e => e.classList.remove('filter_container_dark'))
+          document.querySelectorAll('.filter_container_wrapper').forEach(e => e.classList.remove('filter_container_wrapper_dark'))
+          document.querySelectorAll('.limit_header').forEach(e => e.classList.remove('limit_header_dark'))
+          document.querySelectorAll('.limit_container').forEach(e => e.classList.remove('limit_container_dark'))
           document.querySelectorAll('.button_left').forEach(e => e.classList.remove('button_left_dark'))
           document.querySelectorAll('.button_middle').forEach(e => e.classList.remove('button_middle_dark'))
           document.querySelectorAll('.button_right').forEach(e => e.classList.remove('button_right_dark'))
+          document.querySelectorAll('.p-radiobutton-box').forEach(e => e.classList.remove('p-radiobutton-box_dark'))
+          document.querySelectorAll('.p-accordion-header-link').forEach(e => e.classList.remove('p-accordion-header-link_dark'))
+          document.querySelectorAll('.p-accordion-content').forEach(e => e.classList.remove('p-accordion-content_dark'))
         }
       },
     },
