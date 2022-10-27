02f960d93443bf1378ac0c7c67533a5d90a4d14c
vicecitydeluxe <vikvas95@gmail.com>
2022-10-27 15:53:25 +0400

    -added dark styles to dropdown-finder; -new comments' format in global styles;

--- src/components/CountryMapper.vue
+++ src/components/CountryMapper.vue
@@ -23,7 +23,9 @@
               placeholder="Choose сode"
               :options="countries"
               :filter="true"
+              @change="toggleDarkDropdown"
               @before-show="toggleDarkDropdown"
+              @filter="toggleDarkDropdown"
           />
         </h5>
       </div>
@@ -104,6 +106,9 @@ export default {
             document.querySelectorAll('.p-dropdown-item').forEach(e => e.classList.add('p-dropdown-item_dark'))
             document.querySelectorAll('.p-inputtext').forEach(e => e.classList.add('p-inputtext_dark'))
             document.querySelectorAll('.p-highlight').forEach(e => e.classList.add('p-highlight_dark'))
+            document.querySelectorAll('.p-dropdown-empty-message').forEach(e => e.classList.add('p-dropdown-empty-message_dark'))
+            document.querySelectorAll('.p-dropdown-header').forEach(e => e.classList.add('p-dropdown-header_dark'))
+            document.querySelectorAll('.p-dropdown-filter-icon').forEach(e => e.classList.add('p-dropdown-filter-icon_dark'))
           }, 0)
         }
       }, deep: true
@@ -153,11 +158,16 @@ export default {
           document.querySelectorAll('.p-inputtext').forEach(e => e.classList.add('p-inputtext_dark'))
           document.querySelectorAll('.p-highlight').forEach(e => e.classList.add('p-highlight_dark'))
           document.querySelectorAll('.description').forEach(e => e.classList.add('description_dark'))
+          document.querySelectorAll('.p-dropdown-empty-message').forEach(e => e.classList.add('p-dropdown-empty-message_dark'))
+          document.querySelectorAll('.p-dropdown-header').forEach(e => e.classList.add('p-dropdown-header_dark'))
+          document.querySelectorAll('.p-dropdown-filter-icon').forEach(e => e.classList.add('p-dropdown-filter-icon_dark'))
         }
         if (!newValue) {
           const darkStylesSelectors = ['p-placeholder_dark', 'pi-chevron-down_dark',
             'p-dropdown-item_dark', 'p-inputtext_dark',
-            'p-highlight_dark', 'description_dark']
+            'p-highlight_dark', 'description_dark',
+            'p-dropdown-empty-message_dark', 'p-dropdown-header_dark',
+            'p-dropdown-filter-icon_dark']
           document.querySelectorAll('[class*="_dark"]')
               .forEach(e => e.classList.remove(...darkStylesSelectors))
         }
