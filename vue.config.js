47e1024eed4a11e25caa1abf0f135910b5b74390
vicecitydeluxe <vikvas95@gmail.com>
2022-11-16 15:23:06 +0400

    - quickfix vue.config;

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: '/coldleads-frontend/'
+    publicPath: '/coldleads-frontend/',
+    chainWebpack: config => {
+        config
+            .module
+            .rule('csv')
+            .test(/\.csv$/)
+            .use('file-loader')
+            .loader('file-loader')
+            .options({
+                dynamicTyping: true,
+                header: true,
+                skipEmptyLines: true,
+                name: 'example.csv'
+            })
+            .end()
+    }
 })
 
